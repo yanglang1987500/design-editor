64c515235461f08047ac41008173256b470a4d60
676702008@qq.com <676702008@qq.com>
2019-10-08 11:39:31 +0800

    submit

---
+++ src/store/store.ts
@@ -0,0 +1,16 @@
+
+import { configure } from 'mobx';
+import { IRootStore } from '../schemas/common';
+import DesignState from './DesignState';
+configure({ enforceActions: 'observed' });
+
+class RootStore implements IRootStore {
+
+  DesignState: DesignState;
+
+  constructor() {
+    this['DesignState'] = new DesignState({ rootStore: this });
+  }
+}
+
+export default new RootStore();
